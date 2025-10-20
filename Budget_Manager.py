a57b8d03244281d4888caf1a0c5a0a46d7483d37
IROSHAN DISSANAYAKE <163304289+Iroshan-66@users.noreply.github.com>
2025-10-20 10:56:18 +0530

    created encryption key file part
    next time to develop database

--- Budget_Manager.py
+++ Budget_Manager.py
@@ -1,11 +1,11 @@
 import sqlite3
 import hashlib
 import os
-from cryptography.fernet import fernet
+from cryptography.fernet import Fernet
 import logging
 from getpass import getpass
 
-#---------setup logging---------#
+#---------setup logging---------
 logging.basicConfig(
     filename="budget_app.log",
     level=logging.INFO,
@@ -13,4 +13,21 @@ logging.basicConfig(
 
 )
 
-#----------encrypton key------------#
+#----------encrypton key------------
+#here creating a file for create a encryption key
+
+KEY_FILE = "secret.key"
+if not os.path.exists(KEY_FILE):
+    key = Fernet.generate_key()
+    with open(KEY_FILE,"wb") as kf:
+        kf.write(key)
+else:
+    with open(KEY_FILE,"rb") as kf:
+        key=kf.read()
+
+cipher = Fernet(key)
+
+#------------This is for database setup------------
+
+
+
